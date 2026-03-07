7486692c3b1b69108255bdb0c46841e53a0266c3
HaruSameee <harusamesohuto810@gmail.com>
2026-03-07 23:41:08 +0900

    Switch Drizzle DB client to Neon

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from "drizzle-kit";
 config({ path: ".env.local" });
 
 export default defineConfig({
-  out: "./drizzle",
   schema: "./src/db/schema.ts",
+  out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.POSTGRES_URL ?? "",
+    url: process.env.POSTGRES_URL!,
   },
 });
