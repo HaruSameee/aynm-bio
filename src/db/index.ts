df9e706e92943b0fed89ae2f3bf5bc341406586c
HaruSameee <harusamesohuto810@gmail.com>
2026-03-09 22:01:15 +0900

    ws追加

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,10 @@
-import { Pool } from "@neondatabase/serverless";
+import { Pool, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
+import ws from "ws";
 
 import * as schema from "@/db/schema";
+
+neonConfig.webSocketConstructor = ws;
 
 const pool = new Pool({ connectionString: process.env.POSTGRES_URL! });
 
